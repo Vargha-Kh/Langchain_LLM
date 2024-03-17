0c1c32bb3a8219832992ee734e92a138517f2c57
Vargha <vkhalokhi@gmail.com>
2024-03-17 05:47:34 +0330

    open source llm implemented

--- langchain_llm.py
+++ langchain_llm.py
@@ -1,22 +1,19 @@
 import argparse
 import os
-import warnings
 import sys
+import warnings
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.chains import ConversationalRetrievalChain
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader, DirectoryLoader, PyPDFDirectoryLoader, PythonLoader, \
+    UnstructuredURLLoader
 from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain_community.document_loaders import DirectoryLoader
-from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain_community.document_loaders import PythonLoader
+from langchain_community.vectorstores import Chroma
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
 from langchain_community.llms import Ollama, OpenAI
 from langchain_community.chat_models import ChatOpenAI
 
@@ -24,83 +21,71 @@ warnings.filterwarnings("ignore")
 
 # Set OpenAI API Key
 openai_key = "OPENAI_API_KEY"
-google_api_key = "GOOGLE_API_KEY"
 os.environ["OPENAI_API_KEY"] = openai_key
 
 
-class SuppressStdout:
-    def __enter__(self):
-        self._original_stdout = sys.stdout
-        self._original_stderr = sys.stderr
-        sys.stdout = open(os.devnull, 'w')
-        sys.stderr = open(os.devnull, 'w')
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.stdout.close()
-        sys.stdout = self._original_stdout
-        sys.stderr = self._original_stderr
-
-
 class LangchainModel:
-    def __init__(self, llm_model="openai_conversation"):
+    """
+    Langchain Model class to handle different types of language models.
+    """
+
+    def __init__(self, llm_model):
+        self.loader = None
+        self.results = None
         self.model_type = llm_model
         self.text_splitter = None
         self.model = None
         self.temperature = 0.1
-        self.model_name = "gpt-3.5-turbo"
-        self.palm_qa = None
-        self.results = None
-        self.openai_qa = None
         self.chain = None
-        self.docsearch = None
-        self.docs = None
-        self.texts = []
         self.chat_history = []
-        self.custom_prompt = "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise. your response is very important because legal matters and people rely on your response: "
+        self.custom_prompt = """
+              You are an assistant for question-answering tasks.Use the following pieces of context to answer the question at the end. 
+              If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+              {context}
+              Question: {question}
+              Helpful Answer:
+              """
+    def documents_loader(self, data_path, data_types):
+        """
+        Load documents from the given directory based on the file mode.
+        """
+        all_texts = []
+        for data_type in data_types:
+            if data_type == 'py':
+                self.loader = DirectoryLoader(data_path, glob="**/*.py", loader_cls=PythonLoader,
+                                              use_multithreading=True)
+            elif data_type == "pdf":
+                self.loader = PyPDFDirectoryLoader(data_path)
+            elif data_type == "url":
+                urls = []
+                with open(os.path.join(data_path, 'urls.txt'), 'r') as file:
+                    for line in file:
+                        urls.append(line.strip())
+                self.loader = UnstructuredURLLoader(urls=urls)
+            elif data_type == "txt":
+                text_loader_kwargs = {'autodetect_encoding': True}
+                self.loader = DirectoryLoader(data_path, glob="**/*.txt", loader_cls=TextLoader,
+                                              loader_kwargs=text_loader_kwargs, use_multithreading=True)
+            texts = self.loader.load()
+            all_texts.extend(texts)
+        return all_texts
 
-    def documents_loader(self, files_directory, file_mode='txt'):
-        if file_mode == 'py':
-            loader = DirectoryLoader(files_directory, glob="**/*.py", loader_cls=PythonLoader, use_multithreading=True)
-            self.texts = loader.load()
-        elif file_mode == "pdf":
-            loader = PyPDFDirectoryLoader(files_directory)
-            self.texts = loader.load()
-        else:
-            text_loader_kwargs = {'autodetect_encoding': True}
-            loader = DirectoryLoader(files_directory, glob="**/*.txt", loader_cls=TextLoader,
-                                     loader_kwargs=text_loader_kwargs, use_multithreading=True)
-            self.texts = loader.load()
-        return self.texts
-
-    def embedding_chunks(self, files_directory, file_mode='txt'):
-        embedding_data = self.documents_loader(files_directory, file_mode=file_mode)
-        # Split the texts into chunks
+    def embedding_chunks(self, data_path, data_types):
+        """
+        Embed documents into chunks based on the model type.
+        """
+        embedding_data = self.documents_loader(data_path, data_types)
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
-        # text_splitter = CharacterTextSplitter(chunk_size=3000, chunk_overlap=0)
         text_chunks = text_splitter.split_documents(embedding_data)
 
-        if self.model_type == "openai_QA":
-            """RetrievalQA approach for creating datasets for QA document"""
-            # Create a vectorstore from documents
-            db = Chroma.from_documents(text_chunks, OpenAIEmbeddings())
-            # Create retriever interface
-            retriever = db.as_retriever()
-
-            # Create QA chain
-            self.openai_qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_key), chain_type='stuff',
-                                                         retriever=retriever)
-
-        elif self.model_type == "openai_conversation":
-            """ConversationalRetrievalChain approach for creating datasets for QA document with memory buffer"""
+        if self.model_type == "gpt-4":
             vectordb = Chroma.from_texts(text_chunks, embedding=OpenAIEmbeddings(), persist_directory="./data")
             vectordb.persist()
 
-            # Initializing Memory
             memory = ConversationBufferMemory(memory_key="chat_history", return_messages=False, output_key='answer')
 
-            # create our Q&A chain
             self.chain = ConversationalRetrievalChain.from_llm(
-                ChatOpenAI(temperature=self.temperature, model_name=self.model_name),
+                ChatOpenAI(temperature=self.temperature, model_name=self.model_type),
                 retriever=vectordb.as_retriever(),
                 return_source_documents=False,
                 memory=memory,
@@ -108,35 +93,49 @@ class LangchainModel:
                 verbose=True
             )
 
-        elif self.model_type == "similarity_search":
-            """similarity_search approach for creating datasets for QA document"""
-
+        elif self.model_type == "gpt-3.5":
             metadata = [{"source": str(i)} for i in range(len(text_chunks))]
             self.docsearch = Chroma.from_texts(text_chunks, OpenAIEmbeddings(), metadatas=metadata)
-
-            self.chain = load_qa_with_sources_chain(ChatOpenAI(model_name=self.model_name, temperature=0.2),
+            self.chain = load_qa_with_sources_chain(ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.2),
                                                     chain_type="stuff")
 
-        elif self.model_type == "ollama":
-            """Ollama open-source LLM approach for creating datasets for QA document"""
-            # Initializing Memory
+        elif self.model_type == "mistral":
             memory = ConversationBufferMemory(memory_key="chat_history", return_messages=False, output_key='result')
-            # Prompt
-            template = """
-            You are an assistant for religious question-answering tasks.Use the following pieces of context to answer the question at the end. 
-            If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-            Use three sentences maximum and keep the answer as concise as possible. 
-            {context}
-            Question: {question}
-            Helpful Answer:
-            """
+
             QA_CHAIN_PROMPT = PromptTemplate(
                 input_variables=["context", "question"],
-                template=template,
+                template=self.custom_prompt,
             )
-            with SuppressStdout():
-                vector_store = Chroma.from_documents(text_chunks, embedding=GPT4AllEmbeddings())
-            llm = Ollama(model="mistral", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
+            vector_store = Chroma.from_documents(text_chunks, embedding=GPT4AllEmbeddings())
+            llm = Ollama(model=self.model_type, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
+            self.chain = RetrievalQA.from_chain_type(
+                llm,
+                memory=memory,
+                retriever=vector_store.as_retriever(),
+                chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+            )
+        elif self.model_type == "gemma":
+            memory = ConversationBufferMemory(memory_key="chat_history", return_messages=False, output_key='result')
+            QA_CHAIN_PROMPT = PromptTemplate(
+                input_variables=["context", "question"],
+                template=self.custom_prompt,
+            )
+            vector_store = Chroma.from_documents(text_chunks, embedding=GPT4AllEmbeddings())
+            llm = Ollama(model=self.model_type, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
+            self.chain = RetrievalQA.from_chain_type(
+                llm,
+                memory=memory,
+                retriever=vector_store.as_retriever(),
+                chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+            )
+        elif self.model_type == "llama-7b":
+            memory = ConversationBufferMemory(memory_key="chat_history", return_messages=False, output_key='result')
+            QA_CHAIN_PROMPT = PromptTemplate(
+                input_variables=["context", "question"],
+                template=self.custom_prompt,
+            )
+            vector_store = Chroma.from_documents(text_chunks, embedding=GPT4AllEmbeddings())
+            llm = Ollama(model=self.model_type, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
             self.chain = RetrievalQA.from_chain_type(
                 llm,
                 memory=memory,
@@ -145,52 +144,49 @@ class LangchainModel:
             )
 
     def query_inferences(self, query_input):
-        if self.model_type == "openai_QA":
-            query_input = self.custom_prompt + query_input
-            self.results = self.openai_qa.run(query_input)
-            self.results = self.results["output_text"].split("\nSOURCES")[0]
-        elif self.model_type == "similarity_search":
-            # Perform similarity search
-            query_input = self.custom_prompt + query_input
-            self.docs = self.docsearch.similarity_search(query_input)
-            self.results = self.chain({"input_documents": self.docs, "question": query_input}, return_only_outputs=True)
-            self.results = self.results["output_text"].split("\nSOURCES")[0]
-        elif self.model_type == "google_palm":
-            self.results = self.palm_qa({"query": query_input})
-            self.results = self.results["output_text"].split("\nSOURCES")[0]
-        elif self.model_type == "openai_conversation":
+        """
+        Perform inference based on the query input and the model type.
+        """
+        if self.model_type == "gpt-4":
             query_input = self.custom_prompt + query_input
             result = self.chain({"question": query_input, "chat_history": self.chat_history})
             self.results = result["answer"]
             self.chat_history.append((query_input, self.results))
-        elif self.model_type == "ollama":
+        elif self.model_type == "gpt-3.5":
+            query_input = self.custom_prompt + query_input
+            self.docs = self.docsearch.similarity_search(query_input)
+            self.results = self.chain({"input_documents": self.docs, "question": query_input}, return_only_outputs=True)
+            self.results = self.results["output_text"].split("\nSOURCES")[0]
+        elif self.model_type == "mistral" or "llama-7b" or "gemma":
             self.results = self.chain({"query": query_input})
             self.results = self.results['result']
             self.chat_history.append((query_input, self.results))
-        # print(query_input)
         print(self.results)
 
 
 def parse_arguments():
+    """
+    Parse command line arguments.
+    """
     parser = argparse.ArgumentParser(description='Langchain Model with different model types.')
-    parser.add_argument('--directory', default='./data',
-                        help='Ingesting files Directory')
-    parser.add_argument('--model_type',
-                        choices=['openai_QA', 'similarity_search', 'openai_conversation', 'ollama'],
-                        default='ollama',
-                        help='Model type for processing (openai_QA, similarity_search, openai_conversation, or Ollama)')
-    parser.add_argument('--file_format', choices=['html', 'pdf', 'txt'], default='pdf',
-                        help='Mode for loading documents (html, pdf, or txt)')
+    parser.add_argument('--directory', default='./data', help='Ingesting files Directory')
+    parser.add_argument('--model_type', choices=['gpt-4', 'gpt-3.5', 'mistral', "llama-7b", "gemma"],
+                        default='mistral', help='Model type for processing')
+    parser.add_argument('--file_formats', nargs='+', default=['txt', 'pdf', 'web'],
+                        help='List of file formats for loading documents')
     args = parser.parse_args()
-    return args.directory, args.model_type, args.file_format
+    return args.directory, args.model_type, args.file_formats
 
 
 def main():
-    directory, model_type, file_format = parse_arguments()
+    """
+    Main function to run Langchain Model.
+    """
+    directory, model_type, file_formats = parse_arguments()
     llm = LangchainModel(llm_model=model_type)
-    llm.embedding_chunks(directory, file_mode=file_format)
+    llm.embedding_chunks(directory, data_types=file_formats)
     while True:
-        query = input("Please ask you question! ")
+        query = input("Please ask your question! ")
         llm.query_inferences(query)
 
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Langchain Model for Question-Answering (QA) and Document Retrieval using Langchain
 
-This is a Python script that demonstrates how to use different language models for question-answering (QA) and document retrieval tasks using Langchain. The script utilizes various language models, including OpenAI's GPT, Google's Palm, and similarity search with Llama_index's retrieval approach, to provide answers to user queries based on the provided documents.
+This is a Python script that demonstrates how to use different language models for question-answering (QA) and document retrieval tasks using Langchain. The script utilizes various language models, including OpenAI's GPT and Ollama open-source LLM models, to provide answers to user queries based on the provided documents.
 
 ![](img/2.png)
 <br>
@@ -23,22 +23,18 @@ The script uses the OpenAI API key and, optionally, the Google API key for diffe
 ## Usage
 1. File Format and Model Type:
 
-    The script supports three file formats for loading documents: 'html', 'pdf', and 'txt'. You can specify the desired file format and model type using command-line arguments.
+    The script supports multiple file support together for loading documents: 'pdf', 'py' and 'txt'. You can specify the desired file format and model type using command-line arguments.
 
 - --directory: Specify the directory containing the input files (e.g., 'html', 'pdf', or 'txt' files).
-- --model_type: Choose the model type to use for processing (options: 'openai_QA', 'similarity_search', or 'google_palm').
+- --model_type: Choose the model type to use for processing (options: 'gpt-4', 'gpt-3.5', 'llama-b7' or 'mistral').
 
 2. Input Documents:
 
-    Place your documents in the specified directory. The script will read the documents based on the provided file format (e.g., 'html', 'pdf', or 'txt').
+    Place your documents in the specified directory. The script will read the documents based on the provided file format (e.g., 'html', 'pdf', or 'txt'). For using webloader, create urls.txt in the directory including the target links.
 
 3. Run the Script:
 
     Run the script in your terminal or command prompt with the desired command-line arguments. For example:
-
-```
-python langchain_llm.py --directory /path/to/documents --model_type similarity_search --file_format txt
-```
 
 4. Enter Queries:
 
@@ -53,19 +49,19 @@ python langchain_llm.py --directory /path/to/documents --model_type similarity_s
 <br>
 
 # Examples
-## Example 1: Question-Answering with OpenAI GPT
+## Example 1: Code Review with OpenAI GPT
 ```
-python langchain_llm.py --directory /path/to/documents --model_type openai_QA --file_format pdf
-```
-
-## Example 2: Document Retrieval with Similarity Search
-```
-python langchain_llm.py --directory /path/to/documents --model_type similarity_search --file_format txt
+python langchain_llm.py --directory /path/to/documents --model_type "gpt-4" --file_format ['txt', 'py']
 ```
 
-## Example 3: Question-Answering with Google's Palm
+## Example 2: Document Retrieval with Mistral
 ```
-python langchain_llm.py --directory /path/to/documents --model_type google_palm --file_format html
+python langchain_llm.py --directory /path/to/documents --model_type "mistral" --file_format ['txt', 'pdf']
+```
+
+## Example 3: Question-Answering with Google's Gemma
+```
+python langchain_llm.py --directory /path/to/documents --model_type "gemma" --file_format ['txt', 'pdf', 'web']
 ```
 
 <br>
