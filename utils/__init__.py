34ffa27d7b3a6c7e085c570f5d27b11eb39a6d62
Vargha <vkhalokhi@gmail.com>
2024-05-10 13:16:25 +0330

    adding pinecone, qdrant, faiss, weaviatem milvus vectorstores

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,4 @@ from .multimodal_utils import get_resized_images, resize_base64_image, img_promp
 from .agents import AgenticRAG, AdaptiveRAG, SelfRAG, Reflexion, PlannerAgent, CodeAssistant, WebVoyager
 from .tools import *
 from .prompt_templates import get_prompt
-from .vectorstores import documents_loader, chroma_embeddings, milvus_embeddings, weaviate_embeddings, qdrant_embeddings, pinecone_embeddings, faiss_embeddings
+from .vectorstores import documents_loader, chroma_embeddings, milvus_embeddings, weaviate_embeddings, qdrant_embeddings, pinecone_embeddings, faiss_embeddings, gpt_vision_embeddings
